a5d401725ebe433647fd8406a9640daee72a8fdc
itsmesid12 <74685231+itsmesid12@users.noreply.github.com>
2022-01-21 12:14:53 +0530

    Add files via upload

---
+++ facedet.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  2 14:15:22 2022
+
+@author: Administrator
+"""
+
+import cv2
+
+def opencam(d):
+    cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+    if not cap.isOpened():
+        raise IOError("Cannot open webcam")
+    try:
+        while d:
+            ret1, frame1 = cap.read()
+            frame1 = cv2.resize(frame1, None, fx=1.0, fy=1.0, interpolation=cv2.INTERSECT_FULL)
+            #frame1 = cv2.applyColorMap(frame1,cv2.COLORMAP_RAINBOW)
+            faces = face_cascade.detectMultiScale(frame1,1.1,4)
+            for(x,y,w,h) in faces:
+                cv2.rectangle(frame1,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.imshow('Facerecog',frame1)
+            cv2.waitKey(1)
+            if cv2.getWindowProperty('Facerecog', cv2.WND_PROP_VISIBLE) <1:
+                break
+    except KeyboardInterrupt:
+        cap.release()
+        cv2.destroyAllWindows()
+
+opencam(1)
+
