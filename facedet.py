e0199eb86156eb73ffec73a6c89e36c3e0af8aa2
itsmesid12 <74685231+itsmesid12@users.noreply.github.com>
2022-01-21 13:06:03 +0530

    Update facedet.py

--- facedet.py
+++ facedet.py
@@ -2,29 +2,46 @@
 """
 Created on Sun Jan  2 14:15:22 2022
 
-@author: Administrator
+@author: Sriram Sidhartha R
 """
-
+#incluing opencv
 import cv2
 
 def opencam(d):
+    #open web camera and capturing video
     cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+    
+    #haar casecade is a classifier which helps in detection of objects in the images and videos. 
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+    
+    #checking camera is work or not
     if not cap.isOpened():
         raise IOError("Cannot open webcam")
     try:
         while d:
+            #reading and returning video frame by frame
             ret1, frame1 = cap.read()
+            
+            #take a frame and resizing it
             frame1 = cv2.resize(frame1, None, fx=1.0, fy=1.0, interpolation=cv2.INTERSECT_FULL)
             #frame1 = cv2.applyColorMap(frame1,cv2.COLORMAP_RAINBOW)
+            
+            #Help in finding the face using detectmultiscale
             faces = face_cascade.detectMultiScale(frame1,1.1,4)
+            
+            #Highlighting the face with rectangle of color(0,255,0) that is green rectangle colours can be changed by altering these (0-255,0-255,0-255) 
             for(x,y,w,h) in faces:
                 cv2.rectangle(frame1,(x,y),(x+w,y+h),(0,255,0),2)
-            cv2.imshow('Facerecog',frame1)
+            
+            #showing the frame with face detection
+            cv2.imshow('Facedet',frame1)
             cv2.waitKey(1)
-            if cv2.getWindowProperty('Facerecog', cv2.WND_PROP_VISIBLE) <1:
+            
+            #the below line of codes help in closing the while loop through clicking onto the 'X' in the top right corner
+            if cv2.getWindowProperty('Facedet', cv2.WND_PROP_VISIBLE) <1:
                 break
     except KeyboardInterrupt:
+        #It can also be stopped using Ctrl + C.
         cap.release()
         cv2.destroyAllWindows()
 
